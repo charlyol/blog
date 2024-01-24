1ff9848007ff8a8494ea900b219702ba49551603
charlyol <147928338+charlyol@users.noreply.github.com>
2024-01-24 13:59:32 +0100

    database.php

---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php
+$user = 'olinger';
+$pass = 'Cash2023*';
+
+
+
+try
+{
+    $db = new PDO ('mysql:host=blog.local;dbname=25DEVOPS_2_BDD_CO_1', $user, $pass);
+    foreach ($db->query('SELECT * FROM Categories') as $row)
+    {
+        print_r($row);
+    }
+}
+catch (PDOException $e)
+{
+    print "Erreur :" . $e->getMessage() . "<br/>";
+    die;
+}
+
+?>
