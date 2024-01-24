92e37831571e4529b411393cb740f109ec5a3685
charlyol <147928338+charlyol@users.noreply.github.com>
2024-01-24 13:14:12 +0100

    v1.3 contenant la structure des dossiers et le front controller

--- public/index.php
+++ public/index.php
@@ -1,5 +1,12 @@
-<?php 
+<?php
 echo 'Bienvenue sur le blog'
-?>
 
-<h1>Test</h1>
+$action = filter_input(INPUT_GET, 'action');
+
+if ($action == 'PageN°1') {
+    require '/lienPageN°1';
+} elseif ($action == '/pageN°2') {
+    require 'lienPageN°2';
+} else require 'ressources/views/errors/404.php';
+
+?>
